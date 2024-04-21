9871b0f9f87f402b85c4ef360259341937551164
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2024-04-21 10:30:05 +0800

    21 April 2024

--- 0001 .java
+++ 0001 .java
@@ -1,46 +1,38 @@
-package Quest;
+package April2024;
 
-import java.util.Arrays;
-import java.util.HashMap;
-
-public class Quest0001 {
+public class Tgl21A {
 
     public static void main(String[] args) {
-//        Contoh 1:
-//        Input: Array = [2,7,11,15], target = 9
-//        Output: [0,1]
-//        Explanation: Karena array[0] + array[1] == 9, return [0, 1].
+        int batasAkhir = 5;
 
-//        Contoh 2:
-//        Input: Array = [3,2,4], target = 6
-//        Output: [1,2]
-
-//        Contoh 3:
-//        Input: Array = [3,3], target = 6
-//        Output: [0,1]
-        
-        Solution s = new Solution();
-        int[] arrayAngka1 = {2, 7, 11, 15};        
-        System.out.println(Arrays.toString(s.twoSum(arrayAngka1, 9))); // 0,1
-        int[] arrayAngka2 = {3,2,4};        
-        System.out.println(Arrays.toString(s.twoSum(arrayAngka2, 6))); // 1,2
-        int[] arrayAngka3 = {3,3};        
-        System.out.println(Arrays.toString(s.twoSum(arrayAngka3, 6))); // 0,1
-    }
-}
-
-class Solution {
-
-    public int[] twoSum(int[] nums, int target) {
-        HashMap <Integer,Integer> hashmap = new HashMap <>();
-        for (int i = 0; i < nums.length; i++) {
-            int hasil = target - nums[i];
-            if (hashmap.containsKey(hasil)) {
-                return new int[]{hashmap.get(hasil),i};
+        int count = 0;
+        int bilPrima = 0;
+        int sum = 0;
+        while (count < batasAkhir) {
+            if (cekPrima(bilPrima)) {
+                if (count > 0 && count < batasAkhir) {
+                    System.out.print(" + ");
+                }
+                System.out.print(bilPrima);
+                sum += bilPrima;
+                count++;
             }
-            hashmap.put(nums[i],i);
+            bilPrima++;
         }
-        return null;
+        System.out.print(" = " + sum);
+        System.out.println("");
+
+    }
+
+    static boolean cekPrima(int n) {
+        if (n < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
-
