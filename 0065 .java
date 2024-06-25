4037fe2707859c95d4b3af5fd826d7beeea58941
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2024-06-25 22:56:37 +0800

    HackerRank - Java Generics

---
+++ 0065 .java
@@ -0,0 +1,33 @@
+package HackerRank;
+
+import java.lang.reflect.Method;
+
+public class JavaGenerics {
+
+    public static void main(String[] args) {
+        Printer myPrinter = new Printer();
+        Integer[] intArray = {1, 2, 3};
+        String[] stringArray = {"Hello", "World"};
+        myPrinter.printArray(intArray);
+        myPrinter.printArray(stringArray);
+        int count = 0;
+        for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+            if (name.equals("printArray")) {
+                count++;
+            }
+        }
+        if (count > 1) {
+            System.out.println("Method yang berlebihan tidak diperbolehkan!");
+        }
+    }
+}
+
+class Printer {
+
+    public void printArray(Object[] arr) {
+        for (Object o : arr) {
+            System.out.println(o);
+        }
+    }
+}
